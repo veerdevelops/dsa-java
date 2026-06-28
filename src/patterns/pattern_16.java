009558478dfe25365690478051c91dd2c4702af3
Veer <veergurnani20@gmail.com>
2026-06-28 18:38:09 +0530

    Add pattern 16 solution

---
+++ src/patterns/pattern_16.java
@@ -0,0 +1,12 @@
+package patterns;
+
+public class pattern_16 {
+     public static void main(String[] args) {
+        int n=5;
+         for (int i = 1; i <= n; i++) {
+             for (int j = 1; j <= i; j++) {
+                 System.out.print((char)('A' + i - 1) + " ");
+             }
+             System.out.println();
+    }
+}}
