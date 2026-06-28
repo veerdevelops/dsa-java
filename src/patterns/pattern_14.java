9fbb262e72b725269b391fc42141f55c6fdcf311
Veer <veergurnani20@gmail.com>
2026-06-28 17:32:48 +0530

    Add pattern 14 solution

---
+++ src/patterns/pattern_14.java
@@ -0,0 +1,16 @@
+package patterns;
+
+public class pattern_14 {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print((char)('A' + j-1)+ " ");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
