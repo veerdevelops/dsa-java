4167d3cac74155ffa0126edd25aed09916451e45
Veer <veergurnani20@gmail.com>
2026-06-29 14:27:38 +0530

    Add pattern 17 solution

---
+++ src/patterns/pattern_17.java
@@ -0,0 +1,17 @@
+package patterns;
+
+public class pattern_17 {
+     public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print((char)('A'+j-1)+ " ");
+
+            }
+            for(int j = i-1;j>=1;j--){
+                System.out.print((char)('A'+j-1)+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
