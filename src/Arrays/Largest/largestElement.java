0808d13fe5a324d3d9406b68b42af79867238c14
Veer <veergurnani20@gmail.com>
2026-06-29 15:14:24 +0530

    Largest element in an array

---
+++ src/Arrays/Largest/largestElement.java
@@ -0,0 +1,14 @@
+package Arrays.Largest;
+
+public class largestElement {
+    public static void main(String[] args) {
+        int[]arr ={10,20,5,100,30};
+        int max =arr[0];//assuming fst ele is largest
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i]>max){//is currnt ele > max
+                max = arr[i];
+            }
+        }
+        System.out.println("Largest Element: " + max);
+    }
+}
