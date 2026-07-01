e1d9989549ad783f9099bb69a11280bfb5dee128
Veer <veergurnani20@gmail.com>
2026-07-01 17:31:44 +0530

    remove duplicates

---
+++ src/Arrays/remve_duplicates.java
@@ -0,0 +1,16 @@
+package Arrays;
+//using two pointer
+public class remve_duplicates {
+    public static void main(String[] args) {
+        int []nums ={1,1,2,2,2,3,4,4};
+        int i =0;
+        for (int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        System.out.println("Number of unique elements: " + (i + 1));
+    }
+
+}
