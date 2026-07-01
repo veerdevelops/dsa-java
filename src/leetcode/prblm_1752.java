0b3b4da81dd17a205ab6ea516916ddec28491d16
Veer <veergurnani20@gmail.com>
2026-07-01 15:53:50 +0530

    lc problm 1752 solved

---
+++ src/leetcode/prblm_1752.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class prblm_1752 {
+    public static void main(String[] args) {
+     int [] nums = {3,7,5,6,2};
+     int count =0;
+     for (int i=0;i<nums.length;i++){
+         if (nums[i] > nums[(i+1)% nums.length]) {//use ts for calc in first n last ele
+             count++;
+         }
+     }
+     System.out.println(count <=1);
+    }
+}
