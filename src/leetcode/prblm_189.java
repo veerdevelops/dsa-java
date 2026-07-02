48669ff147f5f5716eb4d548d5a464d4511f8c33
Veer <veergurnani20@gmail.com>
2026-07-02 18:58:37 +0530

    right rotate

---
+++ src/leetcode/prblm_189.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class prblm_189 {
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,4,5,6,7};
+        int k=3;
+        int n = nums.length;
+        int []temp = new int [k];
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[n - k + i];
+    }
+        for (int i = n - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+
+
+
+}
+       System.out.print("Array after right rotation by " + k + ": ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }}}
