980576c6af37a6eebef7f3065c46315b454aa5e2
Veer <veergurnani20@gmail.com>
2026-07-02 16:16:13 +0530

    left rotate

--- src/Arrays/left_Rotate.java
+++ src/Arrays/left_Rotate.java
@@ -2,6 +2,15 @@ package Arrays;
 
 public class left_Rotate {
     public static void main(String[] args) {
+       int []arr = {1,2,3,4,5};
+       int temp= arr[0];
+       for (int i=0; i<arr.length-1;i++){
+           arr[i]=arr[i+1];
 
+       }
+       arr[arr.length-1]=temp;
+        System.out.print("Array after left rotation: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
     }
-}
+}}
