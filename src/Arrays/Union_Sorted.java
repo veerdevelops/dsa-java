72b089f9515b0ad245f2b1a4d3f2cda4b5ba49d7
Veer <veergurnani20@gmail.com>
2026-07-05 17:13:51 +0530

    union sorted Arr

--- src/Arrays/Union_Sorted.java
+++ src/Arrays/Union_Sorted.java
@@ -1,9 +1,67 @@
-package Arrays;//optimal sol
-import java.util.Arrays;
-//union ka mtlb incl every unique element,result sorted hota hai
-public class Union_Sorted {//in 2 diff arrays
+package Arrays;
+
+import java.util.ArrayList;
+
+public class Union_Sorted {
+
+    public static ArrayList<Integer> findUnion(int[] arr1, int[] arr2) {
+
+        int n = arr1.length;
+        int m = arr2.length;
+
+        int i = 0;
+        int j = 0;
+
+        ArrayList<Integer> union = new ArrayList<>();
+
+        while (i < n && j < m) {
+
+            if (arr1[i] <= arr2[j]) {
+
+                if (union.isEmpty() || union.get(union.size() - 1) != arr1[i]) {
+                    union.add(arr1[i]);
+                }
+
+                i++;
+
+            } else {
+
+                if (union.isEmpty() || union.get(union.size() - 1) != arr2[j]) {
+                    union.add(arr2[j]);
+                }
+
+                j++;
+            }
+        }
+
+        while (i < n) {
+
+            if (union.isEmpty() || union.get(union.size() - 1) != arr1[i]) {
+                union.add(arr1[i]);
+            }
+
+            i++;
+        }
+
+        while (j < m) {
+
+            if (union.isEmpty() || union.get(union.size() - 1) != arr2[j]) {
+                union.add(arr2[j]);
+            }
+
+            j++;
+        }
+
+        return union;
+    }
+
     public static void main(String[] args) {
 
+        int[] arr1 = {1, 2, 2, 3, 4};
+        int[] arr2 = {2, 3, 5};
 
+        ArrayList<Integer> result = findUnion(arr1, arr2);
+
+        System.out.println(result);
     }
 }
