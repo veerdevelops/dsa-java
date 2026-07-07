85a5f2f2c03f038bdfd2505bc5b8bedbb0ec3fb9
Veer <veergurnani20@gmail.com>
2026-07-07 17:27:48 +0530

    Hashmap

---
+++ src/leetcode/prblm_136.java
@@ -0,0 +1,35 @@
+package leetcode;
+// hashmap hai bhai key value
+import java.util.HashMap;
+
+public class prblm_136 {
+
+    public static int singleNumber(int[] nums) {
+
+        // Create a HashMap to store frequency
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Store frequency of each element
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Find the element whose frequency is 1
+        for (int num : nums) {
+            if (map.get(num) == 1) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {4, 1, 2, 1, 2};
+
+        int result = singleNumber(nums);
+
+        System.out.println("Single Number: " + result);
+    }
+}
