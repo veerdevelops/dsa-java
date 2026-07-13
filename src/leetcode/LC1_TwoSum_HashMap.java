96acc0791c68bb4799cf3c85cfeffafd094f60d7
Veer <veergurnani20@gmail.com>
2026-07-13 19:31:50 +0530

    OS SOLN

--- src/leetcode/LC1_TwoSum_HashMap.java
+++ src/leetcode/LC1_TwoSum_HashMap.java
@@ -2,7 +2,7 @@ package leetcode;
 
 import java.util.HashMap;
 
-public class prblm_1optimal {
+public class LC1_TwoSum_HashMap {
     public static void main(String[] args) {
         int []nums = {2,7,11,15};
         int target = 9;
