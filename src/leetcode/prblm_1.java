80f58ab935695e67f92ceed3cba4790624d804b0
Veer <veergurnani20@gmail.com>
2026-07-13 18:51:09 +0530

    TWO SUM

---
+++ src/leetcode/prblm_1.java
@@ -0,0 +1,17 @@
+package leetcode;
+//brute force approach hai {not optimised}
+public class prblm_1 {
+    public static void main(String[] args) {
+        int []nums = {2,7,11,15};
+        int target =9;
+        for(int i=0;i<nums.length;i++){
+            for (int j = i+1;j<nums.length;j++){
+                if (nums[i]+nums[j]==target){
+                    System.out.println("Indices: "+i +", "+j);
+                    return;
+                }
+            }
+        }
+        System.out.println("No solution found.");
+    }
+}
