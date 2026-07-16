bbe2f910e6a0ae62f6f3e978654e21ac1c5ab612
Veer <veergurnani20@gmail.com>
2026-07-16 11:41:05 +0530

    PREFIX

---
+++ src/Arrays/PrefixSum.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class PrefixSum{
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+
+        // Build prefix sum directly in nums
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];//updates the current ele nums i by adding previous prefix sum
+        }
+
+        // Print prefix sum array
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
