a820448b9b728a450b0210642f3ea08ab5c001b5
Veer <veergurnani20@gmail.com>
2026-07-23 19:17:17 +0530

    Letter Pyramid

--- src/patterns/pattern16_LetterPyramid.java
+++ src/patterns/pattern16_LetterPyramid.java
@@ -1,6 +1,6 @@
 package patterns;
 
-public class pattern_16 {
+public class pattern16_LetterPyramid {
      public static void main(String[] args) {
         int n=5;
          for (int i = 1; i <= n; i++) {
