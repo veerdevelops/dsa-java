95ef4b4ea7284c1e3028842224699a3272c2ca8c
Veer <veergurnani20@gmail.com>
2026-07-23 19:18:21 +0530

    Pallindrome Pyramid

--- src/patterns/pattern17_PalindromePyramid.java
+++ src/patterns/pattern17_PalindromePyramid.java
@@ -1,6 +1,6 @@
 package patterns;
 
-public class pattern_17 {
+public class pattern17_PalindromePyramid {
      public static void main(String[] args) {
         int n=4;
         for(int i=1;i<=n;i++){
