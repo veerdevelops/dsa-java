0493711875131929f9d5b4002c18b528c91bf7f7
Veer <veergurnani20@gmail.com>
2026-07-25 19:53:04 +0530

    profit n loss

--- src/leetcode/LC122_BestTimeToBuySellStock2.java
+++ src/leetcode/LC122_BestTimeToBuySellStock2.java
@@ -2,6 +2,14 @@ package leetcode;
 
 public class LC122_BestTimeToBuySellStock2 {
     public static void main(String[] args) {
+        int[]prices = {7,1,5,3,6,4};
+        int totalProfit =0;
+        for (int i =1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){//todays prices is greater then yesterdays proice
+                totalProfit+=prices[i]-prices[i-1];//profit = today-yesterday
+            }
+        }
+        System.out.println("total profit :" + totalProfit);
 
     }
 }
