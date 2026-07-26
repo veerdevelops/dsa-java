dca5637684e775b6b369027b1dc6113023f974a7
Veer <veergurnani20@gmail.com>
2026-07-26 18:22:13 +0530

    Rearrangement

---
+++ src/leetcode/LC2149_RearrangeArrayElementBySign.java
@@ -0,0 +1,25 @@
+package leetcode;
+//brute force is already optimal BIG O Complexity is already optimised just an little more space memory diffrence doesnt make a shit of diffrence
+public class LC2149_RearrangeArrayElementBySign {
+    public static void main(String[] args) {
+        int []nums = {3,1,-2,-5};
+        int []result = new int[nums.length];//new array to store the answer
+        int postIndex =0;//postive number will be placed
+        int negIndex=1;//negative numbers will be placed
+        for(int num:nums){//every element
+            if(num>0){//if current elemnt is +ve
+                result[postIndex]=num;
+                postIndex+=2;
+
+            }else{
+                result[negIndex]=num;//the number is negative
+                negIndex+=2;//move to the next odd index
+            }
+        }
+        for (int num : result){
+            System.out.print(num + " ");//print result
+        }
+
+
+    }
+}
